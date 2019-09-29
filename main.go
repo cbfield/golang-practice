3e4ff99792f8f56f836578855b9542b3cc27820a
Christopher Basketfield <chris.basketfield@gmail.com>
2019-09-28 23:15:04 -0800

    string conversion

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main(){
 
 	var x int = 15
+	var y string = strconv.Itoa(x)
 
-	var y float32 = float32(x)
 	fmt.Printf("%v, %T\n",x,x)
 	fmt.Printf("%v, %T\n",y,y)
 }
