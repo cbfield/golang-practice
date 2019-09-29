3e6c0b42276454865786a91c7b9ad3b7b592a8e5
Christopher Basketfield <chris.basketfield@gmail.com>
2019-09-28 23:34:32 -0800

    int operations

--- main.go
+++ main.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main(){
 
-	var x int = 15
-	var y string = strconv.Itoa(x)
+	var x int = 8 //1000
+	var y int = 2 //0010
 
-	fmt.Printf("%v, %T\n",x,x)
-	fmt.Printf("%v, %T\n",y,y)
+	fmt.Println(x + y)
+	fmt.Println(x - y)
+	fmt.Println(x * y)
+	fmt.Println(x / y)
+	fmt.Println(x % y)
+
+	fmt.Println()
+
+	fmt.Println(x & y) 	//0000
+	fmt.Println(x | y) 	//1010
+	fmt.Println(x ^ y)	//1010
+	fmt.Println(x &^ y)	//1000
+
+	fmt.Println()
+
+	fmt.Println(x << 3)
+	fmt.Println(x >> 3)
 }
