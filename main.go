77eb59639b6b77ef49af128c9eac1e14c0621927
Christopher Basketfield <chris.basketfield@gmail.com>
2019-09-28 22:58:07 -0800

    three different ways to declare variables

--- main.go
+++ main.go
@@ -6,5 +6,14 @@ import (
 
 func main(){
 
-	fmt.Println("Hello World!")
+	var num1 int
+	num1 = 1
+
+	var num2 int = 2
+	
+	num3 := 3
+
+	fmt.Printf("%v, %T\n", num1,num1)
+	fmt.Printf("%v, %T\n", num2,num2)
+	fmt.Printf("%v, %T\n", num3,num3)
 }
